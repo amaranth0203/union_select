c0c94d710a48ac476d4c713375306bc65d05499a
戚云琥 <qiyunhu@qiyunhudeMacBook-Pro.local>
2019-11-16 06:14:16 +0800

    修改数据库名称

--- files/flag.sh
+++ files/flag.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # 修改数据库中的 FLAG
-#mysql -e "CREATE DATABASE IF NOT EXISTS supersqli;USE supersqli; CREATE TABLE IF NOT EXISTS \`1919810931114514\` (\`flag\` varchar(100) NOT NULL) ENGINE=MyISAM  DEFAULT CHARSET=utf8;INSERT INTO \`1919810931114514\` VALUES ('$FLAG');" -uroot -proot
-mysql -e "CREATE DATABASE IF NOT EXISTS '$FLAG';" -uroot -proot
+mysql -e "CREATE DATABASE IF NOT EXISTS gxxd;USE gxxd; CREATE TABLE IF NOT EXISTS \`chengji\` (\`flag\` varchar(100) NOT NULL) ENGINE=MyISAM  DEFAULT CHARSET=utf8;INSERT INTO \`chengji\` VALUES ('$FLAG');" -uroot -proot
 
 export FLAG=not_flag
 FLAG=not_flag
--- files/html/index.php
+++ files/html/index.php
@@ -14,19 +14,9 @@
 
 <pre>
 <?php
-//function waf1($inject) {
-//    preg_match("/select|update|delete|drop|insert|where|\./i",$inject) && die('return preg_match("/select|update|delete|drop|insert|where|\./i",$inject);');
-//}
-
-//function waf2($inject) {
-//    strstr($inject, "set") && strstr($inject, "prepare") && die('strstr($inject, "set") && strstr($inject, "prepare")');
-//}
-
 if(isset($_GET['inject'])) {
     $id = $_GET['inject'];
-//    waf1($id);
-//    waf2($id);
-    $mysqli = new mysqli("127.0.0.1","root","root","supersqli");
+    $mysqli = new mysqli("127.0.0.1","root","root","gxxd");
     //多条sql语句
     $sql = "select * from `words` where id = '$id';";
     echo "当前执行语句：".$sql."<br><hr>";
@@ -44,24 +34,7 @@ if(isset($_GET['inject'])) {
     var_dump($result->fetch_assoc());
     echo "<br>";
 
-//    $res = $mysqli->multi_query($sql);
-
-//    if ($res){//使用multi_query()执行一条或多条sql语句
-//      do{
-//        if ($rs = $mysqli->store_result()){//store_result()方法获取第一条sql语句查询结果
-//          while ($row = $rs->fetch_row()){
-//            var_dump($row);
-//            echo "<br>";
-//          }
-//          $rs->Close(); //关闭结果集
-//          if ($mysqli->more_results()){  //判断是否还有更多结果集
-//            echo "<hr>";
-//          }
-//        }
-//      }while($mysqli->next_result()); //next_result()方法获取下一结果集，返回bool值
-//    } else {
-//      echo "error ".$mysqli->errno." : ".$mysqli->error;
-//    }
+    $result->free();
     $mysqli->close();  //关闭数据库连接
 }
 
