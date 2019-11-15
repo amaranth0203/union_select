765478846517d65cdcf839a4972d1eadbf549430
戚云琥 <qiyunhu@qiyunhudeMacBook-Pro.local>
2019-11-16 05:31:34 +0800

    修改php连接数据库代码

--- files/flag.sh
+++ files/flag.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # 修改数据库中的 FLAG
-mysql -e "CREATE DATABASE IF NOT EXISTS supersqli;USE supersqli; CREATE TABLE IF NOT EXISTS \`1919810931114514\` (\`flag\` varchar(100) NOT NULL) ENGINE=MyISAM  DEFAULT CHARSET=utf8;INSERT INTO \`1919810931114514\` VALUES ('$FLAG');" -uroot -proot
+#mysql -e "CREATE DATABASE IF NOT EXISTS supersqli;USE supersqli; CREATE TABLE IF NOT EXISTS \`1919810931114514\` (\`flag\` varchar(100) NOT NULL) ENGINE=MyISAM  DEFAULT CHARSET=utf8;INSERT INTO \`1919810931114514\` VALUES ('$FLAG');" -uroot -proot
+mysql -e "CREATE DATABASE IF NOT EXISTS '$FLAG';" -uroot -proot
 
 export FLAG=not_flag
 FLAG=not_flag
--- files/html/index.php
+++ files/html/index.php
@@ -29,25 +29,38 @@ if(isset($_GET['inject'])) {
     $mysqli = new mysqli("127.0.0.1","root","root","supersqli");
     //多条sql语句
     $sql = "select * from `words` where id = '$id';";
-
-    $res = $mysqli->multi_query($sql);
-
-    if ($res){//使用multi_query()执行一条或多条sql语句
-      do{
-        if ($rs = $mysqli->store_result()){//store_result()方法获取第一条sql语句查询结果
-          while ($row = $rs->fetch_row()){
-            var_dump($row);
-            echo "<br>";
-          }
-          $rs->Close(); //关闭结果集
-          if ($mysqli->more_results()){  //判断是否还有更多结果集
-            echo "<hr>";
-          }
-        }
-      }while($mysqli->next_result()); //next_result()方法获取下一结果集，返回bool值
-    } else {
-      echo "error ".$mysqli->errno." : ".$mysqli->error;
+    echo "当前执行语句：".$sql."<br><hr>";
+    if (!$result = $mysqli->query($sql)) {
+        echo "Errno: " . $mysqli->errno . "\n";
+        echo "Error: " . $mysqli->error . "\n";
+        exit;
     }
+    if ($result->num_rows === 0) {
+        echo "We could not find a match for ID $id, sorry about that. Please try again.";
+        exit;
+    }
+    echo "var_dump(\$mysqli->query($sql))):";
+    var_dump($result);
+    echo "<br>";
+
+//    $res = $mysqli->multi_query($sql);
+
+//    if ($res){//使用multi_query()执行一条或多条sql语句
+//      do{
+//        if ($rs = $mysqli->store_result()){//store_result()方法获取第一条sql语句查询结果
+//          while ($row = $rs->fetch_row()){
+//            var_dump($row);
+//            echo "<br>";
+//          }
+//          $rs->Close(); //关闭结果集
+//          if ($mysqli->more_results()){  //判断是否还有更多结果集
+//            echo "<hr>";
+//          }
+//        }
+//      }while($mysqli->next_result()); //next_result()方法获取下一结果集，返回bool值
+//    } else {
+//      echo "error ".$mysqli->errno." : ".$mysqli->error;
+//    }
     $mysqli->close();  //关闭数据库连接
 }
 
