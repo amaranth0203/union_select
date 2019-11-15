f39bfda512c224bb18acb39c00d0cb1e4604eeb1
戚云琥 <qiyunhu@qiyunhudeMacBook-Pro.local>
2019-11-16 04:55:00 +0800

    去掉 waf

--- files/html/index.php
+++ files/html/index.php
@@ -6,10 +6,7 @@
 </head>
 
 <body>
-  <h1>在SQL中，分号（;）是用来表示一条sql语句的结束。试想一下我们在 ; 结束一个sql语句后继续构造下一条语句，会不会一起执行？因此这个想法也就造就了堆叠注入。而union injection（联合注入）也是将两条语句合并在一起，两者之间有什么区别么？区别就在于union 或者union all执行的语句类型是有限的，可以用来执行查询语句，而堆叠注入可以执行的是任意的语句。例如以下这个例子。用户输入：1; DELETE FROM products服务器端生成的sql语句为：（因未对输入的参数进行过滤）Select * from products where productid=1;DELETE FROM products当执行查询后，第一条显示查询信息，第二条则将整个表进行删除。</h1>
-  <h1>----</h1>
-  <h1>因此如果你不小心破坏了表结构，请重新生成实例</h1>
-<!-- sqlmap是没有灵魂的 -->
+  <h1>sqlmap是没有灵魂的</h1>
 <form method="get">
     姿势: <input type="text" name="inject" value="1">
     <input type="submit">
@@ -17,19 +14,19 @@
 
 <pre>
 <?php
-function waf1($inject) {
-    preg_match("/select|update|delete|drop|insert|where|\./i",$inject) && die('return preg_match("/select|update|delete|drop|insert|where|\./i",$inject);');
-}
+//function waf1($inject) {
+//    preg_match("/select|update|delete|drop|insert|where|\./i",$inject) && die('return preg_match("/select|update|delete|drop|insert|where|\./i",$inject);');
+//}
 
-function waf2($inject) {
-    strstr($inject, "set") && strstr($inject, "prepare") && die('strstr($inject, "set") && strstr($inject, "prepare")');
-}
+//function waf2($inject) {
+//    strstr($inject, "set") && strstr($inject, "prepare") && die('strstr($inject, "set") && strstr($inject, "prepare")');
+//}
 
 if(isset($_GET['inject'])) {
     $id = $_GET['inject'];
-    waf1($id);
-    waf2($id);
-    $mysqli = new mysqli("127.0.0.1","root","root","supersqli");
+//    waf1($id);
+//    waf2($id);
+    $mysqli = new mysqli("127.0.0.1","root","root","ctftraining");
     //多条sql语句
     $sql = "select * from `words` where id = '$id';";
 
